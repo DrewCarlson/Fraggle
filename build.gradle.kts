a4f7ce6b2f3160aaa1947f6191b6cf0426073bc2
Drew Carlson <awcarlson@protonmail.com>
2026-02-03 13:24:15 +0000

    Tests workflow and kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktor) apply false
     alias(libs.plugins.shadow) apply false
+    alias(libs.plugins.kover)
+}
+
+dependencies {
+    kover(project(":app"))
+    kover(project(":fraggle"))
+    kover(project(":fraggle-signal"))
+    kover(project(":fraggle-skills"))
+    kover(project(":backend"))
 }
